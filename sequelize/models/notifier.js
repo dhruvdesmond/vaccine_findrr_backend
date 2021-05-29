5e43349fdc687f7f2942fcb71b2083c7b8bfd7b0
Dhruv Singh <dhruvdesmond@gmail.com>
2021-05-29 19:01:39 +0530

    reset unique emial in notifications

--- sequelize/models/notifier.js
+++ sequelize/models/notifier.js
@@ -15,8 +15,7 @@ module.exports = (sequelize) => {
 		},
 		email: {
 			type: Sequelize.STRING,
-			allowNull: false,
-			unique: true
+			allowNull: false
 		},
 		district_id: {
 			type: Sequelize.INTEGER
