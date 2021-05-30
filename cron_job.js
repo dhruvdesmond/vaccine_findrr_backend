b6a5f843e4a60405e1e48801324c7875faa66a50
Dhruv Singh <dhruvdesmond@gmail.com>
2021-05-30 17:46:38 +0530

    stop force sync

--- cron_job.js
+++ cron_job.js
@@ -34,8 +34,8 @@ async function assertDatabaseConnectionOk() {
 	console.log(`Checking database connection...`);
 	try {
 		await sequelize.authenticate();
-		// await sequelize.sync();
-		await sequelize.sync({force: true});
+		await sequelize.sync();
+		// await sequelize.sync({force: true});
 		console.log('Database connection OK!');
 	} catch (error) {
 		console.log('Unable to connect to the database:');
